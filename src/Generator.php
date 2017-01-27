05a424479b3ad6fc2acf3817b27f00a58368eb9c
melvyn <melvyn@melange.digital>
2017-01-27 19:17:38 +0100

    reindent

--- src/Generator.php
+++ src/Generator.php
@@ -4,86 +4,80 @@ namespace Mmarigny\NameGenerator;
 
 class Generator
 {
-	protected $dir = __DIR__ . '/names'; 
+    protected $dir = __DIR__ . '/names';
 
-	protected $countryCodes = [];
+    protected $countryCodes = [];
 
-	protected $data = [];
+    protected $data = [];
 
-	protected function readFile($filename)
-	{
-		$file =  $this->dir . '/' . $filename;
+    protected function readFile($filename)
+    {
+        $file = $this->dir . '/' . $filename;
 
-		if (false == file_exists($file))
-			throw new \Exception('Unfound file : ' . $file);
+        if (false == file_exists($file))
+            throw new \Exception('Unfound file : ' . $file);
 
-		return explode("\n", file_get_contents($file));
-	}
+        return explode("\n", file_get_contents($file));
+    }
 
 
-	public function loadCountryCode($countryCode) 
-	{
-		if (false == isset($this->data[$countryCode]))
-			$this->data[$countryCode] = [];
+    public function loadCountryCode($countryCode)
+    {
+        if (false == isset($this->data[$countryCode]))
+            $this->data[$countryCode] = [];
 
-		if (empty($this->data[$countryCode]))
-		{
+        if (empty($this->data[$countryCode])) {
             $this->data[$countryCode]['lastname'] = $this->readFile($countryCode . '/' . 'lastname.txt');
             $this->data[$countryCode]['female'] = $this->readFile($countryCode . '/' . 'female.txt');
             $this->data[$countryCode]['male'] = $this->readFile($countryCode . '/' . 'male.txt');
-		}
+        }
 
-		return $this;
-	}
-
-
-	public function load()
-	{
-		if (empty($this->countryCodes))
-		{
-			$this->countryCodes = array_values(array_diff(scandir($this->dir), array('..', '.')));
-
-			if (empty($this->countryCodes))
-				throw new \Exception("No any name directory found");
-		}
-
-		foreach($this->countryCodes as $cc)
-		{
-			$this->loadCountryCode($cc);
-		}
-
-		return $this;
-	}
+        return $this;
+    }
 
 
-	public function getName($countryCode = "", $gender = '')
-	{
-		if (false == empty($countryCode))	// get a name for a given cc
-		{
-			if (empty($this->data[$countryCode]))
-				$this->loadCountryCode($countryCode);
-		} 
-		else 								// get a name in all loaded cc
-		{
-			if (empty($this->data))
-				$this->load();
+    public function load()
+    {
+        if (empty($this->countryCodes)) {
+            $this->countryCodes = array_values(array_diff(scandir($this->dir), array('..', '.')));
+
+            if (empty($this->countryCodes))
+                throw new \Exception("No any name directory found");
+        }
+
+        foreach ($this->countryCodes as $cc) {
+            $this->loadCountryCode($cc);
+        }
+
+        return $this;
+    }
+
+
+    public function getName($countryCode = "", $gender = '')
+    {
+        if (false == empty($countryCode))    // get a name for a given cc
+        {
+            if (empty($this->data[$countryCode]))
+                $this->loadCountryCode($countryCode);
+        } else                                // get a name in all loaded cc
+        {
+            if (empty($this->data))
+                $this->load();
 
             $rand = rand(0, count($this->countryCodes) - 1);
 
-			$countryCode = $this->countryCodes[$rand];
-		}
+            $countryCode = $this->countryCodes[$rand];
+        }
 
-        if ('female' != $gender && 'male' != $gender)
-        {
+        if ('female' != $gender && 'male' != $gender) {
             $gender = rand(0, 1) ? 'female' : 'male';
         }
 
-		$ok = count($this->data[$countryCode][$gender]);
+        $ok = count($this->data[$countryCode][$gender]);
 
         $lastname = $this->data[$countryCode]['lastname'][rand(0, count($this->data[$countryCode]['lastname']) - 1)];
-        $firstname = $this->data[$countryCode][$gender][rand(0, 
-        	count($this->data[$countryCode][$gender]) - 1)
-        ];
+        $firstname = $this->data[$countryCode][$gender][rand(0,
+            count($this->data[$countryCode][$gender]) - 1)];
 
         return [
             'firstname' => $firstname,
@@ -91,13 +85,13 @@ class Generator
             'gender' => $gender,
             'country' => $countryCode,
         ];
-	}
+    }
 
-	public function setDir($dir)
-	{
-		$this->dir = $dir;
+    public function setDir($dir)
+    {
+        $this->dir = $dir;
 
-		return $this;
-	}
+        return $this;
+    }
 
 }
